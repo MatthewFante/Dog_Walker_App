866d64670c08f6a8cd36f1230a4e95fb67fe8308
Matthew Fante <matthew.fante@gmail.com>
2019-05-12 13:28:54 -0400

    created branch to work on passing booking info through url to separate confirmation page

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ const header = document.querySelector('.headerBlank');
 function headerContents(){
     var html = "";
     html += '<h1><a class="mainHeadLink" href="index.html">Walk A Goodboi&trade;</a></h1>';
-    html += '<h2>Dog Walking Service</h2>';
+    html += '<h3>Dog Walking Service</h3>';
     html += '<ul class="main-nav">';
     html += '<li><a href="booking.html">Book a Walker</a></li>';
     html += '<li><a href="contact.html">Contact Us</a></li>';
@@ -27,7 +27,7 @@ function confirm(){
     html += 'Your appointment request for the following has been received for: </br>';
     html += appt_date.value + '&nbsp;&nbsp;' + appt_time.value + '</br>';
     html += 'You will receive a confirmation email shortly.'
-    html += '</br><a href="index.html" class="homeLink">Go Home.</a>';
+    html += '</br></br><a href="index.html" class="homeLink">Go Home</a>';
     return html;
 };
 
