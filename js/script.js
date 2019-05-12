2d67583cf4e23d836f761a73aec4b850ca76beb6
Matthew Fante <matthew.fante@gmail.com>
2019-05-12 11:44:11 -0400

    Separated app into two html files for pagination and updated appoinment form. Also implements media queries to improve appearance in larger browsers.

--- js/script.js
+++ js/script.js
@@ -1,36 +1,40 @@
-const showBookingForm = document.querySelector("#bookWalker");
+
 const bookingForm = document.querySelector('.booking');
-const ourMission = document.querySelector('.ourMission');
 const scheduleConfirm = document.querySelector('#schedule');
 const confirmation = document.querySelector('.confirmation');
 const confirm_name = document.querySelector('#owner_name');
-const resetPage = document.querySelector('.resetDiv');
+const appt_date = document.querySelector('#date');
+const appt_time = document.querySelector('#time');
+const header = document.querySelector('.headerBlank');
 
+function headerContents(){
+    var html = "";
+    html += '<h1><a class="mainHeadLink" href="index.html">Walk A Goodboi&trade;</a></h1>';
+    html += '<h2>Dog Walking Service</h2>';
+    html += '<ul class="main-nav">';
+    html += '<li><a href="booking.html">Book a Walker</a></li>';
+    html += '<li><a href="#">Contact Us</a></li>';
+    html += '</ul>';
+    return html;
+};
 
-showBookingForm.addEventListener('click', () => {
-    ourMission.style.display = 'none';
-    bookingForm.style.display = 'block';
-});
+header.innerHTML = headerContents();
 
 function confirm(){
     var html = "";
     html += 'Thanks, ' + confirm_name.value + '!</br>';
-    html += 'Your appointment has been confirmed!';
-    html += '<div class="resetDiv"><a href="#">Go Home.</a></div>';
+
+    html += 'Your appointment request for the following has been received for: </br>';
+    html += appt_date.value + '&nbsp;&nbsp;' + appt_time.value + '</br>';
+    html += 'You will receive a confirmation email shortly.'
+    html += '<a href="index.html" class="homeLink">Go Home.</a>';
     return html;
 };
 
 scheduleConfirm.addEventListener('click', () => {
-    ourMission.style.display = 'none'
     bookingForm.style.display = 'none';
     confirmation.style.display = 'block';
     confirmation.innerHTML = confirm();
-    resetPage.style.display = 'block';
 });
 
 
-resetPage.addEventListener('click', () => {
-    ourMission.style.display = 'block';
-    bookingForm.style.display = 'none';
-    confirmation.style.display = 'none';
-});
