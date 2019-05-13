7d2aea5e5272d9a185d3b6b94873ac13e6ba9349
Matthew Fante <matthew@Matthews-iMac.local>
2019-05-13 16:59:55 -0400

    Fixed confirmation page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 const bookingForm = document.querySelector('.booking');
 const scheduleConfirm = document.querySelector('#schedule');
-const confirmation = document.querySelector('.confirmation');
+// const confirmation = document.querySelector('.confirmation');
 const confirm_name = document.querySelector('#owner_name');
 const appt_date = document.querySelector('#date');
 const appt_time = document.querySelector('#time');
@@ -20,21 +20,21 @@ function headerContents(){
 
 header.innerHTML = headerContents();
 
-function confirm(){
-    var html = "";
-    html += 'Thanks, ' + confirm_name.value + '!</br>';
+// function confirm(){
+//     var html = "";
+//     html += 'Thanks, ' + confirm_name.value + '!</br>';
 
-    html += 'Your appointment request for the following has been received for: </br>';
-    html += appt_date.value + '&nbsp;&nbsp;' + appt_time.value + '</br>';
-    html += 'You will receive a confirmation email shortly.'
-    html += '</br></br><a href="index.html" class="homeLink">Go Home</a>';
-    return html;
-};
+//     html += 'Your appointment request for the following has been received for: </br>';
+//     html += appt_date.value + '&nbsp;&nbsp;' + appt_time.value + '</br>';
+//     html += 'You will receive a confirmation email shortly.'
+//     html += '</br></br><a href="index.html" class="homeLink">Go Home</a>';
+//     return html;
+// };
 
-scheduleConfirm.addEventListener('click', () => {
-    bookingForm.style.display = 'none';
-    confirmation.style.display = 'block';
-    confirmation.innerHTML = confirm();
-});
+// scheduleConfirm.addEventListener('click', () => {
+//     bookingForm.style.display = 'none';
+//     confirmation.style.display = 'block';
+//     confirmation.innerHTML = confirm();
+// });
 
 
