de0c8c98feeb17dc3fdd659de07964aecf5e8db4
Matthew Fante <matthew.fante@gmail.com>
2019-05-13 17:49:55 -0400

    Footer contents are now filled by script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
-
-const bookingForm = document.querySelector('.booking');
-const scheduleConfirm = document.querySelector('#schedule');
-// const confirmation = document.querySelector('.confirmation');
-const confirm_name = document.querySelector('#owner_name');
-const appt_date = document.querySelector('#date');
-const appt_time = document.querySelector('#time');
 const header = document.querySelector('.headerBlank');
+const footer = document.querySelector('.footerBlank');
+
+// Adjust contents of header and footer here
 
 function headerContents(){
     var html = "";
@@ -17,24 +13,13 @@ function headerContents(){
     html += '</ul>';
     return html;
 };
-
 header.innerHTML = headerContents();
 
-// function confirm(){
-//     var html = "";
-//     html += 'Thanks, ' + confirm_name.value + '!</br>';
 
-//     html += 'Your appointment request for the following has been received for: </br>';
-//     html += appt_date.value + '&nbsp;&nbsp;' + appt_time.value + '</br>';
-//     html += 'You will receive a confirmation email shortly.'
-//     html += '</br></br><a href="index.html" class="homeLink">Go Home</a>';
-//     return html;
-// };
+function footerContents(){
+    var html = "";
+    html += "<small><a href='https://github.com/MatthewFante'>&copy; Matthew Fante 2019</a></small>"
+    return html;
+};
 
-// scheduleConfirm.addEventListener('click', () => {
-//     bookingForm.style.display = 'none';
-//     confirmation.style.display = 'block';
-//     confirmation.innerHTML = confirm();
-// });
-
-
+footer.innerHTML = footerContents();
