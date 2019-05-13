9a5dc9159458f87d7100b8ee7fa93e967f7a2329
Matthew Fante <matthew.fante@gmail.com>
2019-05-13 19:46:11 -0400

    Cleanup

--- js/confirmation.js
+++ js/confirmation.js
@@ -8,13 +8,14 @@ var searchParams = new URLSearchParams(window.location.search);
 // Extract the info needed for the confirmation
 var ownerName = searchParams.get('owner_name');
 var dogName = searchParams.get('dog_name');
+var email = searchParams.get('user_email');
 
 //Create the HTML content for the confirmation and place in on the page
 function confirm(){
     var html = "";
-    html += 'Thanks, ' + ownerName + '!</br>';
-    html += 'Your appointment request for ' + dogName + '</br>has been received.</br>';
-    html += 'You will receive a confirmation email shortly.'
+    html += 'Thanks, ' + ownerName + '!</br></br>';
+    html += 'Your appointment request for ' + dogName + '</br>has been received.</br></br>';
+    html += 'You will receive a confirmation</br>email shortly at </br>' + email;
     html += '</br></br><a href="index.html" class="homeLink">Go Home</a>';
     return html;
 };
